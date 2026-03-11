3d1afc66e0559db0ea5b60b528fd3840d8b6d1cf
The embodiment of disharmony <34@rncyk.org>
2026-03-12 04:32:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="stbl",
+    name="stbdl",
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        "stbl": ["*.exe", "*.png"],
+        "stbdl": ["*.exe", "*.png"],
     },
     exclude_package_data={
         "": ["*.rc"],
@@ -16,7 +16,7 @@ setup(
     description="A simple taskbar decoration library",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/theembodimentofdisharmony/stbl",
+    url="https://github.com/theembodimentofdisharmony/stbdl",
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -24,4 +24,5 @@ setup(
         "Operating System :: Microsoft :: Windows"
     ],
     python_requires=">=3.0",
+
 )
